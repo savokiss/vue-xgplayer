fa86d80530eae2379f340545aad46e65f5f99e99
savokiss <jaynaruto@qq.com>
2018-09-11 16:01:14 +0800

    fix: treat xgplayer as external

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  baseUrl: isProduction ? '/vue-xgplayer' : '/'
+  baseUrl: isProduction ? '/vue-xgplayer' : '/',
+  configureWebpack: {
+    externals: {
+      xgplayer: 'xgplayer',
+      nanoid: 'nanoid'
+    }
+  }
 }
